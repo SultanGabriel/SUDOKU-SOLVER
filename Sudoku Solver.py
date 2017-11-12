f0f65cc0e15243552ad13eddcc6186ade6e5cca7
Sultan <sultangabriel75@yahoo.com>
2017-11-12 01:57:53 +0100

    getting it done
    added check square, getting the numbers, and started to backtrack

--- Sudoku Solver.py
+++ Sudoku Solver.py
@@ -11,72 +11,123 @@ Sdk = deepcopy(OSdk)
 
 def drawGrid():
     for i in range(8):
-        c.create_line(0, (i + 1) * 50, WIDTH, (i + 1) * 50, fill="black")
-        c.create_line((i + 1) * 50, 0, (i + 1) * 50, HEIGHT, fill="black")
+        w = 1
+        if i == 2 or i == 5:
+            w = 3
+        c.create_line(0, (i + 1) * 50, WIDTH, (i + 1) * 50, fill="black", width=w)
+        c.create_line((i + 1) * 50, 0, (i + 1) * 50, HEIGHT, fill="black", width=w)
 
 
 def drawNum():
-    for i in range(9):
-        for j in range(9):
-            if Sdk[i][j] != 0:
-                if Sdk[i][j] == OSdk[i][j]:
-                    c.create_text(j * 50 + 25, i * 50 + 25, font=("Arial", 20), text=Sdk[i][j], fill="red")
+    for y in range(9):
+        for x in range(9):
+            if Sdk[y][x] != 0:
+                if Sdk[y][x] == OSdk[y][x]:
+                    c.create_text(x * 50 + 25, y * 50 + 25, font=("Arial", 20), text=Sdk[y][x], fill="red")
                 else:
-                    c.create_text(j * 50 + 25, i * 50 + 25, font=("Arial", 20), text=Sdk[i][j], fill="black")
+                    c.create_text(x * 50 + 25, y * 50 + 25, font=("Arial", 20), text=Sdk[y][x], fill="black")
 
 
 def draw():
-    c.delete("all")
-    drawNum()
+    # c.delete("all")
     drawGrid()
+    drawNum()
 
-
-def checkRow(n, j):
+def checkRow(n, _x, y):
     for x in range(9):
-        if n == Sdk[j][x]:
+        if n == Sdk[y][x] and x != _x:
             return False
     return True
 
 
-def checkCol(n, i):
-    for x in range(9):
-        if n == Sdk[x][i]:
+def checkCol(n, x, _y):
+    for y in range(9):
+        if n == Sdk[y][x] and y != _y:
             return False
     return True
 
 
-def checkSq(n, i, j):
-    if int(j / 3) == 0:
-        if int(i / 3) == 0:
-            for x in range(3):
-                for y in range(3):
-                    if n == Sdk[y][x]:
-                        return False
-            return True
+def checkSq(n, x, y):
+    minX = int(x / 3) * 3
+    minY = int(y / 3) * 3
 
-        if int(i / 3) == 1:
-            for x in range(3):
-                for y in range(3, 6):
-                    print(y,x)
-                    if n == Sdk[y][x]:
-                        return False
-            return True
+    maxX = (int(x / 3) + 1) * 3
+    maxY = (int(y / 3) + 1) * 3
 
-        if int(i / 3) == 2:
-            for x in range(3):
-                for y in range(6, 9):
-                    print(x,y)
-                    if n == Sdk[y][x]:
-                        return False
-            return True
+    for y in range(minY, maxY):
+        for x in range(minX, maxX):
+            if n == Sdk[y][x]:
+                return False
+    return True
 
 
-##print(checkSq(3, 3,0))
+def check(n, x, y):
+    if checkRow(n, x, y) and checkCol(n, x, y) and checkSq(n, x, y):
+        return True
+    return False
 
 
-def my_mainloop():
-    draw()
+def nums():
+    for y in range(9):
+        for x in range(9):
+            for n in range(1, 10):
+                if OSdk[y][x] == 0 and check(n, x, y):
+                    Sdk[y][x] = n
+                    break
+
+whiteSp = []
+def backtrack():
+    for y in range(9):
+        for x in range(9):
+            if Sdk[y][x] == 0:
+                whiteSp.append((y,x))
+                c.create_oval(x * 50 + 2, y * 50 + 2, x * 50 + 48, y * 50 + 48)
+
+    while len(whiteSp) != 0:
+        for idx in range(len(whiteSp)):
+            x = whiteSp[idx][0]
+            y = whiteSp[idx][1]
+            rowN = []
+            colN = []
+            sqN = []
+
+            for n in range(9):
+                if checkRow(n, x, y):
+                    rowN.append(n)
+
+                if checkCol(n, x, y):
+                    colN.append(n)
+
+                if checkSq(n, x, y):
+                    sqN.append(n)
+
+            while True:
+                iRow = 0
+                iCol = 0
+                iSq = 0
+
+                # TODO find a way to itterate through all possible combinations
+
+                if rowN[iRow] == colN[iCol] and colN[iCol] == sqN[iSq]:
+                    Sdk[x][y] = n
 
 
-master.after(40, my_mainloop)
+                break
+
+
+
+nums()
+backtrack()
+print(whiteSp)
+
+# def my_mainloop():
+draw()
+
+# master.after(40, my_mainloop)
 master.mainloop()
+
+#TODO resolve the x y / y x  problem
+#TODO add a better mode to change the numbers
+#TODO add some button or thing to solve the sudoku
+#TODO add a menu !
+
