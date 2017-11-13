9e725df6d583755531bde0fc73fbccb869ea946c
Sultan <sultangabriel75@yahoo.com>
2017-11-14 00:44:00 +0100

    21e

---
+++ Back-up/Sudoku Solver.py
@@ -0,0 +1,141 @@
+from tkinter import *
+from vars import *
+from copy import deepcopy
+
+master = Tk()
+c = Canvas(master, height=HEIGHT, width=WIDTH)
+c.pack()
+
+Sdk = deepcopy(OSdk)
+
+
+def drawGrid():
+    for i in range(8):
+        w = 1
+        if i == 2 or i == 5:
+            w = 3
+        c.create_line(0, (i + 1) * 50, WIDTH, (i + 1) * 50, fill="black", width=w)
+        c.create_line((i + 1) * 50, 0, (i + 1) * 50, HEIGHT, fill="black", width=w)
+
+
+def drawNum():
+    for y in range(9):
+        for x in range(9):
+            if Sdk[y][x] != 0:
+                if Sdk[y][x] == OSdk[y][x]:
+                    c.create_text(x * 50 + 25, y * 50 + 25, font=("Arial", 20), text=Sdk[y][x], fill="red")
+                else:
+                    c.create_text(x * 50 + 25, y * 50 + 25, font=("Arial", 20), text=Sdk[y][x], fill="black")
+
+
+def draw():
+    # c.delete("all")
+    drawGrid()
+    drawNum()
+
+
+def checkRow(n, _x, y):
+    for x in range(9):
+        if n == Sdk[y][x] and x != _x:
+            return False
+    return True
+
+
+def checkCol(n, x, _y):
+    for y in range(9):
+        if n == Sdk[y][x] and y != _y:
+            return False
+    return True
+
+
+def checkSq(n, x, y):
+    minX = int(x / 3) * 3
+    minY = int(y / 3) * 3
+
+    maxX = (int(x / 3) + 1) * 3
+    maxY = (int(y / 3) + 1) * 3
+
+    for y in range(minY, maxY):
+        for x in range(minX, maxX):
+            if n == Sdk[y][x]:
+                return False
+    return True
+
+
+def check(n, x, y):
+    if checkRow(n, x, y) and checkCol(n, x, y) and checkSq(n, x, y):
+        return True
+    return False
+
+
+def nums():
+    for y in range(9):
+        for x in range(9):
+            for n in range(1, 10):
+                if OSdk[y][x] == 0 and check(n, x, y):
+                    Sdk[y][x] = n
+                    break
+
+
+whiteSp = []
+
+
+def backtrack():
+    for y in range(9):
+        for x in range(9):
+            if Sdk[y][x] == 0:
+                whiteSp.append((y, x))
+                c.create_oval(x * 50 + 2, y * 50 + 2, x * 50 + 48, y * 50 + 48)
+
+    while len(whiteSp) != 0:
+        for idx in range(len(whiteSp)):
+            x = whiteSp[idx][0]
+            y = whiteSp[idx][1]
+            rowN = []
+            colN = []
+            sqN = []
+
+            for n in range(9):
+                if checkRow(n, x, y):
+                    rowN.append(n)
+
+                if checkCol(n, x, y):
+                    colN.append(n)
+
+                if checkSq(n, x, y):
+                    sqN.append(n)
+
+            while True:
+                for iRow in len(rowN):
+                    for iCol in len(colN):
+                        for iSq in len(sqN):
+
+                            if rowN[iRow] == colN[iCol] and colN[iCol] == sqN[iSq]:
+                                Sdk[x][y] = rowN[iRow]
+                                break
+
+                            if rowN[iRow] == colN[iCol]:
+                                Sdk[x][y] = rowN[iRow]
+
+                            if colN[iCol] == sqN[iSq]:
+                                Sdk[x][y] == sqN[iSq]
+
+
+
+
+
+nums()
+# backtrack()
+
+print(whiteSp)
+
+# def my_mainloop():
+draw()
+
+# master.after(40, my_mainloop)
+master.mainloop()
+
+# TODO resolve the x y / y x  problem
+# TODO add a better mode to change the numbers
+# TODO add some button or thing to solve the sudoku
+# TODO add a menu !
--- Sudoku Solver.py
+++ Sudoku Solver.py
@@ -1,12 +1,13 @@
-from copy import deepcopy
 from tkinter import *
-from vars import *
-from pprint import pprint
-master = Tk()
-c = Canvas(master, height=HEIGHT, width=WIDTH)
-c.pack()
+import vars as v
+from Algorithm import solveSudoku
 
-Sdk = deepcopy(OSdk)
+v.init()
+
+master = Tk()
+c = Canvas(master, height=v.HEIGHT, width=v.WIDTH)
+
+c.pack()
 
 
 def drawGrid():
@@ -14,18 +15,18 @@ def drawGrid():
         w = 1
         if i == 2 or i == 5:
             w = 3
-        c.create_line(0, (i + 1) * 50, WIDTH, (i + 1) * 50, fill="black", width=w)
-        c.create_line((i + 1) * 50, 0, (i + 1) * 50, HEIGHT, fill="black", width=w)
+        c.create_line(0, (i + 1) * 50, v.WIDTH, (i + 1) * 50, fill="black", width=w)
+        c.create_line((i + 1) * 50, 0, (i + 1) * 50, v.HEIGHT, fill="black", width=w)
 
 
 def drawNum():
     for y in range(9):
         for x in range(9):
-            if Sdk[y][x] != 0:
-                if Sdk[y][x] == OSdk[y][x]:
-                    c.create_text(x * 50 + 25, y * 50 + 25, font=("Arial", 20), text=Sdk[y][x], fill="red")
+            if v.Sdk[y][x] != 0:
+                if v.Sdk[y][x] == v.OSdk[y][x]:
+                    c.create_text(x * 50 + 25, y * 50 + 25, font=("Arial", 20), text=v.Sdk[y][x], fill="red")
                 else:
-                    c.create_text(x * 50 + 25, y * 50 + 25, font=("Arial", 20), text=Sdk[y][x], fill="black")
+                    c.create_text(x * 50 + 25, y * 50 + 25, font=("Arial", 20), text=v.Sdk[y][x], fill="black")
 
 
 def draw():
@@ -33,87 +34,16 @@ def draw():
     drawGrid()
     drawNum()
 
-
-def checkRow(n, _x, y):
-    for x in range(9):
-        if n == Sdk[y][x] and x != _x:
-            return False
-    return True
-
-
-def checkCol(n, x, _y):
-    for y in range(9):
-        if n == Sdk[y][x] and y != _y:
-            return False
-    return True
-
-
-def checkSq(n, x, y):
-    minX = int(x / 3) * 3
-    minY = int(y / 3) * 3
-
-    maxX = (int(x / 3) + 1) * 3
-    maxY = (int(y / 3) + 1) * 3
-
-    for y in range(minY, maxY):
-        for x in range(minX, maxX):
-            if n == Sdk[y][x]:
-                return False
-    return True
-
-
-def check(n, x, y):
-    if checkRow(n, x, y) and checkCol(n, x, y) and checkSq(n, x, y):
-        return True
-    return False
-
-
-def getEMN(x, y):  # getEveryMatchingNumber
-    nms = []
-    for n in range(1, 10):
-        if check(n, x, y):
-            nms.append(n)
-    return nms
-
-
-def checkEN():
-    nums = [[[0] for j in range(9)] for i in range(9)]
-    for y in range(9):
-        for x in range(9):
-            if Sdk[y][x] == 0:
-                nums[y][x] = getEMN(x, y)
-    return nums
-
-
-def setNums():
-    nums = checkEN()
-    for y in range(9):
-        for x in range(9):
-            if len(nums[y][x]) == 1 and nums[y][x][0] != 0:
-                n = nums[y][x][0]
-                Sdk[y][x] = n
-
-def solveSudoku():
-    solved = False
-    while not solved:
-        setNums()
-        for y in range(9):
-            for x in range(9):
-                if Sdk[y][x] == 0:
-                    solved = False
-                    break
-                elif x == 8 and y == 8 and Sdk[y][x] != 0:
-                    solved = True
+def solve():
+    solveSudoku()
 
 def my_mainloop():
     solveSudoku()
-
     draw()
 
 master.after(40, my_mainloop)
 master.mainloop()
 
-# TODO resolve the x y / y x  problem
 # TODO add a better mode to change the numbers
 # TODO add some button or thing to solve the sudoku
 # TODO add a menu !
